a065b642de42cc97e7023d4b6fc06d8c8bd309ab
kropalikhappines <arsik30394@gmail.com>
2023-03-29 16:07:04 +0300

    add html front bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    print(url_for('index'))
+    return render_template('index.html')
+
+
+@app.route('/post_input', methods=['POST'])
+def post_input():
+    print(url_for('post_input'))
+    if request.method == 'POST':
+        print(request.form)
+    return render_template('load.html')
 
 
 @app.route('/about')
